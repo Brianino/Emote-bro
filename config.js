e18f254525b57b33560030a9508972c319b633aa
Brianino <brian.manganaro@gmail.com>
2017-07-19 18:27:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.token = 'MzM1NDczMzI0NzY5NTQyMTQ1.DEqRsw.ZAuUnXNaZQucHWZoPDIsINlhczE';
-config.owner = 233239455874547712;
+config.token = ''/*String for Bot token*/;
+config.owner = ''/*Owner ID*/;
 
 module.exports = config;
