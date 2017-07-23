0f02d5e99d699cb0abf038856d14e31c1253383a
Brianino <brian.manganaro@gmail.com>
2017-07-23 17:01:36 +0100

    added config example

---
+++ config example.js
@@ -0,0 +1,11 @@
+var config = {}
+
+/*
+  *	Config Example
+  *	Create a folder called "Files" within the Emote-Bro directory.
+  *	Rename this file to config.js
+*/
+config.token = ''/*String for Bot token*/;
+config.owner = ''/*Owner ID*/;
+
+module.exports = config;
